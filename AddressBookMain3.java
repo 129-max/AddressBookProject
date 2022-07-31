eda7393f99f7a5aa5583be7c096a02ef315a07f6
Mohit Raj <rajmohit.129@gmail.com>
2022-07-31 12:44:25 +0530

    [Mohit]Added:Function To Edit Contact

---
+++ AddressBookMain3.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class AddressBookMain3 {
+
+    public static void main(String[] args) {
+        AddressBook3 addressBook = new AddressBook3();
+        //calling add method using addressBookMainObject
+        addressBook.getUserChoice();
+    }
+}
